1022334defb81c208c88db2786fae4eb87d89054
Bruno Lima <bruno@fidelize.com.br>
2017-12-05 11:38:36 -0200

    remove port from endpoint configuration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var MESAGISTO_HOST = process.env.MESAGISTO_HOST;
 var MESAGISTO_PORT = (process.env.PORT || process.env.MESAGISTO_PORT);
 const config = {
-    endpoint: "http://" + MESAGISTO_HOST + ":" + MESAGISTO_PORT +  "/bayeux",
+    endpoint: "http://" + MESAGISTO_HOST + "/bayeux",
     publicDir: __dirname + "/public/",
     baseDir: __dirname + "/",
     port: MESAGISTO_PORT,
